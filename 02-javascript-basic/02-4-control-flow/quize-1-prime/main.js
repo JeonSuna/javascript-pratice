aa33ef327de72295b1189138f3b3aee512ef8e98
JeonSuna <jsa020910@gmail.com>
2024-09-10 17:33:39 +0900

    퀴즈해답 수정

--- 02-javascript-basic/02-4-control-flow/quize-1-prime/main.js
+++ 02-javascript-basic/02-4-control-flow/quize-1-prime/main.js
@@ -1,19 +1,44 @@
+// //소수판별 만들기-첫번쨰 시도
+
+// "use strict";
+// const primeTest = () => {
+//     let no = prompt('2이상의 숫자를 입력하세요');
+//     let message = '소수입니다'
+//     if (no < 2 || isNaN(no))
+//         message = '올바르게 입력해주세요'
+//     else {
+//         for (let i = 2; i < no; i++) {
+//             if (no % i === 0) {
+//                 message = '소수가 아닙니다'
+//                 break
+//             }
+//         }
+//     }
+//     //message 는 유저에게 출력하고자 하는 문자열을 저장한 변수
+//     document.querySelector('#result').innerHTML = message
+
+// }
+
+
+//두번재 시도
 "use strict";
-
 const primeTest = () => {
-    let no = prompt('2 이상의 숫자를 입력하세요')
-    no=parseInt(no)
+    let no = prompt('2이상의 숫자를 입력하세요');
     let message = ''
-    for (let i = 2; i < no; i++) {
-        if (no % i === 0) {
-            document.write(`${no}는 소수가아닙니다`)
-            break
+
+    if (no < 2 || isNaN(no))
+        message = '올바르게 입력해주세요'
+    else {
+        let isPrime = true;
+        for (let i = 2; i < no; i++) {
+            if (no % i === 0) {
+                isPrime = false
+                break
+            }
         }
-        
+        message = isPrime ? message = '소수입니다' : message = '소수가 아닙니다'
     }
-    return`${no}는 소수입니다`
-
-
     //message 는 유저에게 출력하고자 하는 문자열을 저장한 변수
     document.querySelector('#result').innerHTML = message
+
 }
--- 02-javascript-basic/02-4-control-flow/quize-2-prime/main.js
+++ 02-javascript-basic/02-4-control-flow/quize-2-prime/main.js
@@ -20,58 +20,58 @@
 //     document.querySelector("#result").innerHTML = message;
 // };
 
-"use strict";
-
-const primeTest = () => {
-    let no = prompt("2 이상의 숫자를 입력하세요");
-    let primes = [];
-    let message = "";
-
-    for (let i = 2; i <= no; i++) {
-        let isPrime = true;
-        for (let j = 2; j <= Math.sqrt(i); j++) {
-            if (i % j === 0) {
-                isPrime = false;
-                break;
-            }
-        }
-        if (isPrime) {
-            primes.push(i);
-        }
-    }
-    message = `입력하신 ${no}까지의 소수는 ${primes.join(", ")}입니다.`;
-
-    //message 는 유저에게 출력하고자 하는 문자열을 저장한 변수
-    document.querySelector("#result").innerHTML = message;
-};
+// "use strict";
 
 // const primeTest = () => {
 //     let no = prompt("2 이상의 숫자를 입력하세요");
-//     no = parseInt(no);
 //     let primes = [];
 //     let message = "";
 
-//     if (no < 2 || isNaN(no)) {
-//         message = "2 이상의 유효한 숫자를 입력해주세요.";
-//     } else {
-//         for (let i = 2; i <= no; i++) {
-//             //숫자가 소수일수도 있고 아닐수도 있고 그 결과를 담기위한 boolean 변수
-//             let isPrime = true;
-//             for (let j = 2; j < i; j++) {
-//                 if (i % j === 0) {
-//                     isPrime = false;
-//                     break;
-//                 }
-//             }
-//             if (isPrime) {
-//                 primes.push(i);
+//     for (let i = 2; i <= no; i++) {
+//         let isPrime = true;
+//         for (let j = 2; j <= Math.sqrt(i); j++) {
+//             if (i % j === 0) {
+//                 isPrime = false;
+//                 break;
 //             }
 //         }
-//         message = `입력하신 ${no}까지의 소수는 ${primes.join(", ")}입니다.`;
+//         if (isPrime) {
+//             primes.push(i);
+//         }
 //     }
+//     message = `입력하신 ${no}까지의 소수는 ${primes.join(", ")}입니다.`;
 
+//     //message 는 유저에게 출력하고자 하는 문자열을 저장한 변수
 //     document.querySelector("#result").innerHTML = message;
 // };
 
-// // 함수 실행
-// primeTest();
+const primeTest = () => {
+    let no = prompt("2 이상의 숫자를 입력하세요");
+    no = parseInt(no);
+    let primes = [];
+    let message = "";
+
+    if (no < 2 || isNaN(no)) {
+        message = "2 이상의 유효한 숫자를 입력해주세요.";
+    } else {
+        for (let i = 2; i <= no; i++) {
+            //숫자가 소수일수도 있고 아닐수도 있고 그 결과를 담기위한 boolean 변수
+            let isPrime = true;
+            for (let j = 2; j < i; j++) {
+                if (i % j === 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                primes.push(i);
+            }
+        }
+        message = `입력하신 ${no}까지의 소수는 ${primes.join(", ")}입니다.`;
+    }
+
+    document.querySelector("#result").innerHTML = message;
+};
+
+// 함수 실행
+primeTest();
