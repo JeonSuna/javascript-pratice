3ca45249fa11c2e08a66f4a350ed0b7cc890c858
JeonSuna <jsa020910@gmail.com>
2024-09-11 14:28:15 +0900

    오타 수정..

--- 02-javascript-basic/02-5-function/quize/main.js
+++ 02-javascript-basic/02-5-function/quize/main.js
@@ -1,15 +1,15 @@
 const calcBonus = (name, arg1, money) => {
-    let new_nmoney = 0
+    let new_money = 0
     if (arg1 === "A") {
-        new_nmoney = money * 0.3
+        new_money = money * 0.3
     }
-    else if (arg1 == "B") {
-        new_nmoney = money * 0.5
+    else if (arg1 === "B") {
+        new_money = money * 0.5
     }
     else {
         new_money = money * 0.1
 
     }
-    console.log(`${name}의 추석 보너스는 ${new_nmoney}입니다`)
+    console.log(`${name}의 추석 보너스는 ${new_money}입니다`)
 }
 calcBonus('전선아', 'A', 1000)
