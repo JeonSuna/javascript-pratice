5014247eedbc129f8f6b6c4f0c969af858100cd9
JeonSuna <jsa020910@gmail.com>
2024-09-15 17:00:14 +0900

    퀴즈 풀이 완료

--- 02-javascript-basic/02-7-event/quize-2/main.js
+++ 02-javascript-basic/02-7-event/quize-2/main.js
@@ -1,47 +1,51 @@
-let form = document.querySelector('#form');
-let input1 = document.querySelector('#input1');
-let input2 = document.querySelector('#input2');
-let result = document.querySelector('#result');
-let error=document.querySelector('#error')
+"use strict"
 
+//valid 상황을 여러함수에서 공유해야 한다면.. global 로.. 
+let isIdValid = false
+let isPwValid = false
+let id = ''
+let pw = ''
 
-function printResult(msg) {
-    result.innerHTML = msg
+function idValidCheck() {
+    let idNode = document.getElementById('id')
+    id = idNode.value
+    let idErrorMsgNode = document.getElementById('idErrorMsg')
+
+    //문자열.trim() - 문자열의 앞뒤 공백 제거.. 
+    if (id == null || id.trim().length == 0) {
+        isIdValid = false
+        idErrorMsgNode.innerHTML = '아이디는 필수 입력입니다.'
+    } else {
+        isIdValid = true
+        idErrorMsgNode.innerHTML = ''
+    }
 }
 
-function handler(e) {
-    printResult(`${e.target.value}`)
+function pwValidCheck() {
+    let pwNode = document.getElementById('pw')
+    pw = pwNode.value
+
+    let pwErrorMsgNode = document.getElementById('pwErrorMsg')
+    let regExpPassword = /^(?=.*[a-zA-Z])(?=.*[0-9]).{6,16}$/
+    console.log(pw)
+    console.log(regExpPassword.test(pw))
+    if (pw == null || pw.trim().length == 0) {
+        isPwValid = false
+        pwErrorMsgNode.innerHTML = '패스워드는 필수 입력입니다.'
+    } else if (!regExpPassword.test(pw)) {
+        isPwValid = false
+        pwErrorMsgNode.innerHTML = '패스워드는 영문자, 숫자 조합에 6자 이상입니다.'
+    } else {
+        isPwValid = true
+        pwErrorMsgNode.innerHTML = ''
+    }
 }
-input1.addEventListener('change', handler)
-    
-function Password(password) {
-    const regex = /^(?=.*[a-zA-A])(?=.*\d)[a-zA-Z\d]{6,}$/;
-    return regex.test(password);
+
+function login() {
+    idValidCheck()
+    pwValidCheck()
+    if (isIdValid && isPwValid) {
+        let resultNode = document.getElementById('result')
+        resultNode.innerHTML = `${id}, ${pw} 로 로그인을 시도합니다.`
+    }
 }
-
-input1.addEventListener('blur', (e) => {
-    if (e.target.value === '') {
-       printResult('아이디는 필수입력 입니다')
-    } else {
-        handler(e)
-    }
-})
-
-input2.addEventListener('blur', (e) => {
-    if (!Password(e.target.value)) {
-        printResult('패스워드는 영문자 숫자 조합 6자 이상이어야 됩니다.');
-    } else {
-        handler(e)
-    }
-});
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    if (input1.value === '') {
-        printResult('아이디는 필수입력 입니다');
-    } else if (!Password(input2.value)) {
-        printResult('패스워드는 영문자 숫자 조합 6자 이상이어야 됩니다.');
-    } else {
-        printResult(`${input1.value},${input2.value}로 로그인을 시도합니다`);
-    }
-});
