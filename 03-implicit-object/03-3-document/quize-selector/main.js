c7909f6eb230e5bdb3dcef10b2060cf857d9195d
JeonSuna <jsa020910@gmail.com>
2024-09-20 14:01:24 +0900

    강사님 퀴즈 풀이

--- 03-implicit-object/03-3-document/quize-selector/main.js
+++ 03-implicit-object/03-3-document/quize-selector/main.js
@@ -1,25 +1,27 @@
 function printResult() {
-    let checkArray=[]
+    let checkArray = []
     let result = document.querySelector('#result')
     let name = document.querySelector('#name')
-    let checkNod = document.getElementsByClassName('hobby')
-   
-    for (let i = 0; i < checkNod.length; i++){
+    let checkNod = document.getElementsByClassName('hobby') //체크박스는 id나 class를 동일하게 준다.그래야 컨트롤이 쉬움
+                                                            //만약 따로따로 주면 하나하나 접근해야해서 귀찮기 때문
+
+    for (let i = 0; i < checkNod.length; i++) {
         if (checkNod[i].checked) {
             checkArray.push(checkNod[i].value)
         }
     }
+
     let radioNode = document.getElementsByName('gender')
-    let radio=''
-    for (let i = 0; i < radioNode.length; i++){
+    let radio = ''
+    for (let i = 0; i < radioNode.length; i++) {
         if (radioNode[i].checked) {
-            radio=radioNode[i].value
+            radio = radioNode[i].value
         }
     }
+    
     result.innerHTML = `
     name: ${name.value} <br>
     취미: ${checkArray} <br>
-    성별:${radio}
+    성별: ${radio}
     `
 }
-
