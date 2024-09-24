001f7d443421c2f44f56d996a07bd278c90ec029
JeonSuna <jsa020910@gmail.com>
2024-09-24 16:03:28 +0900

    플젝 유효성 검증

--- 04-oop/04-2/04-2-1/main.js
+++ 04-oop/04-2/04-2-1/main.js
@@ -40,4 +40,44 @@ let messages = []
 //1씩 증가시켜서 신규 발생 채팅 메세지 식별자 id(msgId)로 사용하기 위해서
 let msgId = 0
 
-//
+//필요한 dom node 객체 획득
+let nicknameInputNode = document.getElementById('nicknameInput')
+let idInputNode = document.getElementById('idInput')
+let msgInputNode = document.getElementById('msgInput')
+//최종 동적으로 양산된 채팅 글이 찍힐 위치
+let chatMainNode = document.getElementById('chat-main')
+
+//매개변수로 전달된 객체의 내용을 node를 만들어서 출력시키는 역할
+function printMessage(msg) {
+    
+}
+
+function send(e) {
+    //유저 입력 데이터 추출
+    e.prventDefault()
+    let id = idInputNode.value
+    let nickname = nicknameInputNode.value
+    let msg = msgInputNode.value
+
+    //유효성 검증
+    if (id.trim().length == 0 || nickname.trim().length == 0 || msg.trim().length == 0) {
+        alert('아이디,닉네임과 메세지를 입력해야 합니다')
+        return
+    } else {
+        
+    }
+
+    //화면에 동적 노드 만들어서 출력
+
+}
+
+function printEmoji(message) {
+    
+}
+
+
+//이모지 추가 클릭 이벤트 처리 
+function emojiClick() {
+    //동적으로 이모지를 메시지에 출력한다
+
+}
